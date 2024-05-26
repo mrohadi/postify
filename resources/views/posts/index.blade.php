e0ace114a1bc793b711374a952cef1b0b1cc1ba6
mrohadi <muhammadrohadi03@gmail.com>
2024-05-26 22:07:03 +0700

    [mrohadi] - feat: implement simple policy based authorization

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -28,18 +28,13 @@
                         <a href="" class="font-bold">{{ $post->user->name }}</a> <span class="text-gray-600 text-sm">{{ $post->created_at->diffForHumans() }}</span>
                         <p class="mb-2">{{ $post->body }}</p>
 
-
-                        @auth
-                            <div>
-                                @if ($post->ownedBy(auth()->user()))
-                                    <form action="{{ route('posts.destroy', $post) }}" method="post" class="mr-1">
-                                        @csrf
-                                        @method('DELETE')
-                                        <button type="submit" class="text-red-500">Delete</button>
-                                    </form>
-                                @endif
-                            </div>
-                        @endauth
+                        @can('delete', $post)
+                            <form action="{{ route('posts.destroy', $post) }}" method="post" class="mr-1">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="text-red-500">Delete</button>
+                            </form>
+                        @endcan
 
                         <div class="flex items-center">
                             @auth
